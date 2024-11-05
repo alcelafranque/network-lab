1c40957cb537525b83dca380642e1755bd5875d2
Alcé Lafranque <alce@lafranque.net>
2024-11-05 17:38:06 +0100

    frr-evpn-vxlan-vrf-leak-ip-unicast: initial commit

---
+++ frr-evpn-vxlan-vrf-leak-ip-unicast/R2/setup.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Dummy
+ip link add dummy0 type dummy
+ip addr add 10.10.10.2/32 dev dummy0
+ip link set up dev dummy0
+
+# P2P to R2
+ip addr add 10.0.0.2/30 dev eth1
+
+
+# Add bridge and single vxlan device
+ip link add vx0 type vxlan local 10.10.10.2 dstport 4789 external vnifilter nolearning
+ip link add br0 type bridge vlan_filtering 1 vlan_stats_enabled 1 vlan_stats_per_port 1
+ip link set vx0 master br0
+ip link set vx0 type bridge_slave vlan_tunnel on neigh_suppress on learning off
+ip link set br0 addr aa:bb:cc:00:00:02
+ip link set br0 up
+ip link set vx0 up
+
+sysctl -qw net.ipv4.conf.vx0.forwarding=1
+sysctl -qw net.ipv6.conf.vx0.forwarding=1
+
+
+# L3 VRF
+ip link add vrf1 type vrf table 100
+ip link add vrf2 type vrf table 200
+ip link set up vrf1
+ip link set up vrf2
+
+ip link add name l3vni100 link br0 type vlan id 100 protocol 802.1q
+ip link add name l3vni200 link br0 type vlan id 200 protocol 802.1q
+bridge vlan add vid 100 dev br0 self
+bridge vlan add vid 200 dev br0 self
+ip link set l3vni100 up
+ip link set l3vni100 master vrf1
+ip link set l3vni200 up
+ip link set l3vni200 master vrf2
+bridge vni add dev vx0 vni 100
+bridge vni add dev vx0 vni 200
+bridge vlan add dev vx0 vid 100 master
+bridge vlan add dev vx0 vid 100 tunnel_info id 100 master
+bridge vlan add dev vx0 vid 200 master
+bridge vlan add dev vx0 vid 200 tunnel_info id 200 master
+
+sysctl -qw net.ipv4.conf.l3vni100.forwarding=1
+sysctl -qw net.ipv6.conf.l3vni100.forwarding=1
+sysctl -qw net.ipv4.conf.l3vni200.forwarding=1
+sysctl -qw net.ipv6.conf.l3vni200.forwarding=1
+
+# L2 VNI 
+ip link add name l2vni110 link br0 type vlan id 110 protocol 802.1q
+bridge vlan add vid 110 dev br0 self
+ip link set l2vni110 up
+ip addr add 10.0.110.1/24 dev l2vni110
+ip link set l2vni110 master vrf1
+sysctl -qw net.ipv4.conf.l2vni110.forwarding=1
+
+ip link add name l2vni120 link br0 type vlan id 120 protocol 802.1q
+bridge vlan add vid 120 dev br0 self
+ip link set l2vni120 up
+ip addr add 10.0.120.1/24 dev l2vni120
+ip link set l2vni120 master vrf1
+sysctl -qw net.ipv4.conf.l2vni120.forwarding=1
+
+ip link add name l2vni220 link br0 type vlan id 220 protocol 802.1q
+bridge vlan add vid 220 dev br0 self
+ip link set l2vni220 up
+ip addr add 10.0.220.1/24 dev l2vni220
+ip link set l2vni220 master vrf2
+sysctl -qw net.ipv4.conf.l2vni220.forwarding=1
+
+# add VNI / VLAN table
+bridge vni add dev vx0 vni 110 
+bridge vlan add dev vx0 vid 110 master
+bridge vlan add dev vx0 vid 110 tunnel_info id 110 master
+
+bridge vni add dev vx0 vni 120
+bridge vlan add dev vx0 vid 120 master
+bridge vlan add dev vx0 vid 120 tunnel_info id 120 master
+
+bridge vni add dev vx0 vni 220
+bridge vlan add dev vx0 vid 220 master
+bridge vlan add dev vx0 vid 220 tunnel_info id 220 master
+
+# PC2
+ip link set eth3 master br0
+bridge vlan add dev eth3 vid 220 master pvid untagged
+
---
+++ frr-evpn-vxlan-vrf-leak-ip-unicast/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker exec clab-frr01-PC1 /setup.sh
+docker exec clab-frr01-PC2 /setup.sh
+
+docker exec clab-frr01-R1 /setup.sh
+docker exec clab-frr01-R2 /setup.sh
+
+docker exec clab-frr01-LB1 /setup.sh
