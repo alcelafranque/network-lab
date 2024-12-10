4168bcc0bb67d218f7f106281bc2f13edab661dd
Alcé Lafranque <alce@lafranque.net>
2024-12-10 10:39:57 +0100

    frr-evpn-vxlan-type-5-onlink: WIP

--- frr-evpn-vxlan-type-5-onlink/R2/setup.sh
+++ frr-evpn-vxlan-type-5-onlink/R2/setup.sh
@@ -2,19 +2,19 @@
 
 # Dummy
 ip link add dummy0 type dummy
-ip addr add 10.0.0.1/32 dev dummy0
+ip addr add 10.0.0.2/32 dev dummy0
 ip link set up dev dummy0
 
 # P2P to R2
-ip addr add 100.64.0.1/30 dev eth1
+ip addr add 100.64.0.2/30 dev eth1
 
 
 # Add bridge and single vxlan device
-ip link add vx0 type vxlan local 10.0.0.1 dstport 4789 external vnifilter nolearning
+ip link add vx0 type vxlan local 10.0.0.2 dstport 4789 external vnifilter nolearning
 ip link add br0 type bridge vlan_filtering 1 vlan_stats_enabled 1 vlan_stats_per_port 1
 ip link set vx0 master br0
 ip link set vx0 type bridge_slave vlan_tunnel on neigh_suppress on learning off
-ip link set br0 addr aa:bb:cc:00:00:01
+ip link set br0 addr aa:bb:cc:00:00:02
 ip link set br0 up
 ip link set vx0 up
 
@@ -24,9 +24,10 @@ sysctl -qw net.ipv6.conf.vx0.forwarding=1
 sysctl -qw net.ipv4.conf.br0.forwarding=1
 sysctl -qw net.ipv6.conf.br0.forwarding=1
 
+
 while read vrf table; do
     ip link add $vrf type vrf table $table
-    ip link set $vrf up
+    ip link set $vrf up 
     ip link add name l3vni$table link br0 type vlan id $table protocol 802.1q
     bridge vlan add vid $table dev br0 self
     ip link set l3vni$table up
@@ -49,8 +50,5 @@ while read iface local_ip vrf nei_ip; do
     ip route add $nei_ip dev $iface proto static vrf $vrf
     sysctl -qw net.ipv4.conf.$iface.forwarding=1
 done <<EOF
-eth2 10.1.0.1/32 purple 10.1.0.2/32
-eth3 10.2.0.1/32 red 10.2.0.2/32
-eth4 10.3.0.1/32 blue 10.3.0.2/32 
+eth3 10.2.0.1/32 red 10.2.0.3/32
 EOF
-
--- frr-evpn-vxlan-type-5-onlink/setup.sh
+++ frr-evpn-vxlan-type-5-onlink/setup.sh
@@ -4,3 +4,15 @@ docker exec clab-frr-evpn-vxlan-type-5-onlink-R1 /setup.sh
 docker exec clab-frr-evpn-vxlan-type-5-onlink-R2 /setup.sh
 
 docker exec clab-frr-evpn-vxlan-type-5-onlink-LB1 /setup.sh
+
+while read container_name ip_address gateway; do
+  docker exec -d "$container_name" sh -c "
+    ip link set eth1 up &&
+    ip addr add $ip_address dev eth1 &&
+    ip route replace default via $gateway dev eth1 onlink
+  "
+done <<EOF
+clab-frr-evpn-vxlan-type-5-onlink-PC1 10.2.0.2/32 10.2.0.1
+clab-frr-evpn-vxlan-type-5-onlink-PC2 10.3.0.2/32 10.3.0.1
+clab-frr-evpn-vxlan-type-5-onlink-PC3 10.2.0.3/32 10.2.0.1
+EOF
